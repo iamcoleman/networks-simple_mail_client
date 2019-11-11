d792e4a2c06f94377743f063230d226138376ca0
Coleman <coleman.lyski@gmail.com>
2019-11-11 16:32:43 -0500

    first commit

---
+++ mail-client.py
@@ -0,0 +1,14 @@
+# SMTP
+import smtplib
+
+# MIMEMultipart Message Object
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ your-mail-login.py
@@ -0,0 +1,8 @@
+"""
+Replace the `yourEmailAddress` and `yourEmailPassword` variables with your information
+"""
+
+def yourMailLogin():
+    yourEmailAddress = "insert your email address"
+    yourEmailPassword = "insert your email password"
+    return yourEmailAddress, yourEmailPassword

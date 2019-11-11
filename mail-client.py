dea29315ceea88f2165df72a59b8aadf384b404a
Coleman <coleman.lyski@gmail.com>
2019-11-11 16:57:58 -0500

    additions

--- mail-client.py
+++ mail-client.py
@@ -5,10 +5,33 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+# Email Validation
+from validate_email import validate_email
+
+
+def getRecipient():
+    yes = {'yes', 'ye', 'y'}
+    while True:
+        print("Please type the recipient's name:")
+        recName = input()
+        print("Please type the recipient's email address:")
+        recAddress = input()
+        print("Is this correct (Y/n)? {} at {}".format(recName, recAddress))
+        choice = input().lower()
+        if choice in yes:
+            if validate_email(recAddress):
+                return recName, recAddress
+            else:
+                print("Email is invalid\n")
+
 
 def main():
-    return
+    # get the name and address of recipient
+    recName, recAddress = getRecipient()
 
+    # setup the SMTP server
+    server = smtplib.SMTP()
+    return
 
 if __name__ == '__main__':
     main()
--- your-mail-login.py
+++ your-mail-login.py
@@ -1,8 +1,15 @@
 """
-Replace the `yourEmailAddress` and `yourEmailPassword` variables with your information
+Your Mail Login = replace the `yourEmailAddress` and `yourEmailPassword` variables with your information
+
+Your Mail SMTP = replace the `yourMailHost` and `yourMailPort` variables with your mail server information
 """
 
 def yourMailLogin():
-    yourEmailAddress = "insert your email address"
-    yourEmailPassword = "insert your email password"
+    yourEmailAddress = "your email address"
+    yourEmailPassword = "your email password"
     return yourEmailAddress, yourEmailPassword
+
+def yourMailSMTP():
+    yourMailHost = "your mail server address"
+    yourMailPort = "your mail port number"
+    return yourMailHost, yourMailPort

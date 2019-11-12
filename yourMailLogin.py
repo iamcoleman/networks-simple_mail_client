f2603a865529448b7d891bf4cf9b786ba2de1eff
Coleman Lyski <coleman.lyski@gmail.com>
2019-11-12 17:52:18 -0500

    add back

---
+++ yourMailLogin.py
@@ -0,0 +1,16 @@
+"""
+Your Mail Login = replace the `your_email_address` and `your_email_password` variables with your information
+Your Mail SMTP = replace the `your_mail_host` and `your_mail_port` variables with your mail server information
+"""
+
+
+def your_mail_login():
+    your_email_address = "your email address"
+    your_email_password = "your email password"
+    return your_email_address, your_email_password
+
+
+def your_mail_smtp():
+    your_mail_host = "your mail server address"
+    your_mail_port = "your mail port number"
+    return your_mail_host, your_mail_port

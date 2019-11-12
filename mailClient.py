d386704fe1eb1a9d142b5c53ef706b38084b50c4
Coleman Lyski <coleman.lyski@gmail.com>
2019-11-12 17:45:14 -0500

    quick update

--- mailClient.py
+++ mailClient.py
@@ -15,7 +15,7 @@ from email.mime.text import MIMEText
 from validate_email import validate_email
 
 
-def getRecipient():
+def get_recipient():
     """
     Returns the recipient name and address from user input
 
@@ -25,19 +25,19 @@ def getRecipient():
     yes = {'yes', 'ye', 'y'}
     while True:
         print("Please type the recipient's name:")
-        recName = input()
+        rec_name = input()
         print("Please type the recipient's email address:")
-        recAddress = input()
-        print("Is this correct (Y/n)? {} at {}".format(recName, recAddress))
+        rec_address = input()
+        print("Is this correct (Y/n)? {} at {}".format(rec_name, rec_address))
         choice = input().lower()
         if choice in yes:
-            if validate_email(recAddress):
-                return recName, recAddress
+            if validate_email(rec_address):
+                return rec_name, rec_address
             else:
-                print("Email ({}) is invalid\n".format(recAddress))
+                print("Email ({}) is invalid\n".format(rec_address))
 
 
-def getMessage(filename):
+def get_message(filename):
     """
     Returns a Template object for the message
 
@@ -52,11 +52,12 @@ def getMessage(filename):
 
 def main():
     # get the name and address of recipient
-    recName, recAddress = getRecipient()
+    rec_name, rec_address = get_recipient()
 
     # get the message to email
-    messageTemplate = getMessage('email_message.txt')
-    message = messageTemplate.substitute(RECIPIENT_NAME=recName)
+    # ${RECIPIENT_NAME} in template will get replaced
+    message_template = get_message('email_message.txt')
+    message = message_template.substitute(RECIPIENT_NAME=rec_name)
     print("\nCreated the message:\n\n{}\n".format(message))
 
     # setup the SMTP server
@@ -79,9 +80,9 @@ def main():
 
     # create the email
     email = MIMEMultipart()
-    email['From']=MY_ADDRESS
-    email['To']=recAddress
-    email['Subject']="Coleman's Mail Client"
+    email['From'] = MY_ADDRESS
+    email['To'] = rec_address
+    email['Subject'] = "Coleman's Mail Client"
     email.attach(MIMEText(message, 'plain'))
 
     # send email with SMTP server
